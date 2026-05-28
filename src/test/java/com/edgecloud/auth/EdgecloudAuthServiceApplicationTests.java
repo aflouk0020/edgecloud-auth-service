3df0f354c4e0539f5e0a9cdec8acea58584940ce
Taha Aflouk <tahaaflouk94@gmail.com>
2026-05-28 23:31:28 +0100

    SCRUM-19 configure auth service database integration

---
+++ src/test/java/com/edgecloud/auth/EdgecloudAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgecloud.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgecloudAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
