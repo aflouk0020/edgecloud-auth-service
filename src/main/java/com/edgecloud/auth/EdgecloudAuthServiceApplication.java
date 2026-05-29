91ec37353f7d6407817f522a2f58f1a2c9aa4855
Taha Aflouk <tahaaflouk94@gmail.com>
2026-05-29 18:11:36 +0100

    SCRUM-21 fix Auth Service Docker build and Eureka configuration

---
+++ src/main/java/com/edgecloud/auth/EdgecloudAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.edgecloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgecloudAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgecloudAuthServiceApplication.class, args);
+    }
+}
